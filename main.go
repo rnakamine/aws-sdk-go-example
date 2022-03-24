1a5365f0f46f22d4fec92fc87a00e0f866f809b3
Ryo Nakamine <rnakamine8080@gmail.com>
2022-03-25 05:46:08 +0900

    Add fetch security group rules

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	profileName := "xxx"
+	session := session.Must(session.NewSessionWithOptions(session.Options{Profile: profileName}))
+	svc := ec2.New(session, aws.NewConfig().WithRegion("ap-northeast-1"))
+	output, err := svc.DescribeSecurityGroupRules(&ec2.DescribeSecurityGroupRulesInput{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var fullOpen []*ec2.SecurityGroupRule
+	for _, rule := range output.SecurityGroupRules {
+		if rule.CidrIpv4 != nil {
+			if *rule.IsEgress == false && *rule.IpProtocol == "tcp" && *rule.CidrIpv4 == "0.0.0.0/0" {
+				fullOpen = append(fullOpen, rule)
+			}
+		}
+	}
+
+	fmt.Println(fullOpen)
 }
